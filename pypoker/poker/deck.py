34ed60200c36ef2511e7dcc9df3f63c18a98c220
Julia Eskew <juliasq@spotify.com>
2023-04-28 11:35:42 -0400

    Remove poker.

--- pypoker/poker/deck.py
+++
@@ -1,8 +0,0 @@
-from poker.card import *
-import itertools
-
-class Deck:
-    suits = ["Spades", "Hearts", "Diamonds", "Clubs"]
-    cards = ["Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine",
-             "Ten", "Jack", "Queen", "King", "Ace"]
-    deck = [Card(item[0], item[1]) for item in itertools.product(cards, suits)]
